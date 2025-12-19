9f8ca440df522834f7ea2c4db7d9a319aee301d2
Adilson <estudosimformatica@gmail.com>
2025-12-18 22:48:41 -0300

    Commit inicial

---
+++ src/main/java/br/com/easy/aalife_api/AalifeApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.easy.aalife_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AalifeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AalifeApiApplication.class, args);
+	}
+
+}
